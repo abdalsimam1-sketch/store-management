ac7076af00d282726004a9b8b7a0431b898d7e7a
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-07-14 13:48:45 +0100

    feat : desktop cashier sidebar

--- client/src/components/cashier/CashierLayout.jsx
+++ client/src/components/cashier/CashierLayout.jsx
@@ -1,3 +1,19 @@
+import CashierSidebar from "./CashierSidebar";
+import { Outlet } from "react-router-dom";
+
 export const CashierLayout = () => {
-  return <div>CashierLayout</div>;
+  return (
+    <div className="d-flex ">
+      <section className="d-none d-md-flex">
+        <CashierSidebar></CashierSidebar>
+      </section>
+      <section className="d-md-none ">
+        {/* <AdminMobileNav></AdminMobileNav> */}
+      </section>
+
+      <section className="w-100">
+        <Outlet></Outlet>
+      </section>
+    </div>
+  );
 };
