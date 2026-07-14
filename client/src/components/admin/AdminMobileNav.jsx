c0ae63ca27d5121d90fad5a51d1775a61e1e65c5
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-07-14 10:30:40 +0100

    chore : define active nav link for mobile

--- client/src/components/admin/AdminMobileNav.jsx
+++ client/src/components/admin/AdminMobileNav.jsx
@@ -1,28 +1,45 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export const AdminMobileNav = () => {
+  const { pathname } = useLocation();
+  const activeLink = (path) => {
+    return pathname === path ? "active-mobile-nav" : "";
+  };
   return (
     <div className="mobile-nav z-1 text-light w-100 rounded-top d-flex justify-content-between">
       <div>
-        <Link to="/admin" className={`btn d-flex flex-column`}>
+        <Link
+          to="/admin"
+          className={`btn d-flex flex-column ${activeLink("/admin")}`}
+        >
           <i className="bi-grid-1x2 "></i>
           <span>Dashboard</span>
         </Link>
       </div>
       <div>
-        <Link to="/admin/products" className={`btn d-flex flex-column`}>
+        <Link
+          to="/admin/products"
+          className={`btn d-flex flex-column ${activeLink("/admin/products")}`}
+        >
           <i className="bi-box-seam "></i>
           <span>Products</span>
         </Link>
       </div>
       <div>
-        <Link to="/admin/cashiers" className={`btn d-flex flex-column`}>
+        <Link
+          to="/admin/cashiers"
+          className={`btn d-flex flex-column ${activeLink("/admin/cashiers")}`}
+        >
           <i className="bi-person "></i>
           <span>Cashiers</span>
         </Link>
       </div>
       <div>
-        <Link to="/admin/reports" className={`btn d-flex flex-column`}>
+        <Link
+          to="/admin/reports"
+          className={`btn d-flex flex-column ${activeLink("/admin/reports")}`}
+        >
           <i className="bi-bar-chart-line "></i>
           <span>Reports</span>
         </Link>
